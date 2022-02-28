5ee89ddfb4c72d60d431ea2f708fbe2df7f475a4
noa.azran <noa.azran@polytechnique.edu>
2022-02-28 17:27:59 +0100

    load vp

--- src/loading.py
+++ src/loading.py
@@ -1,6 +1,9 @@
 import pandas as pd
+from tqdm import tqdm
 import sys
 sys.path.append("../")
+
+YEAR_RANGE = range(2012,2022)
 
 def load_vehicule_data(path = '../data/vp_communes.csv'):
     """Load existing data on historical automobile parc in France at the communal level
@@ -13,14 +16,17 @@ def load_vehicule_data(path = '../data/vp_communes.csv'):
     """ 
     return pd.read_csv(path).drop(columns=['Unnamed: 0'])
 
-def load_demographic_data(path = 'Data/'):
+def load_demographic_data(path = 'Data/demographic.xlsx'):
     """Load demographic data (both historical and projection) of french population at the department level.
-
     Args:
         path (str, optional): Path to data_. Defaults to 'Data/'.
     """
     
-    # TODO
+    df = pd.DataFrame()
     
-    return
-
+    for year in tqdm(YEAR_RANGE):
+        df_year = pd.read_excel(path,sheet_name = str(year), index_col = [0,1], header = [0,1])
+        df_year['year'] = year
+        df = pd.concat([df,df_year])
+    
+    return df
