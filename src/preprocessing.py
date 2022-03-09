e88ee3e105478b5a7f6b03c9583c09d27d8ad34b
lancelotperonnet <lancelot.peronnet@hec.edu>
2022-03-09 16:28:59 +0100

    geo

---
+++ src/preprocessing.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from pyproj import Proj, transform
+
+COLS = ['objetID', 'longueur', 'xD', 'yD', 'xF', 'yF','typeComptageTrafic', 'TMJA', 'ratio_PL' ]
+
+def keep_useful_cols(df, cols=COLS):
+    return df[cols]
+
+
+def transform_coordinates(df):
+    inProj = Proj(init='epsg:2154')
+    outProj = Proj(init='epsg:4326')
+    xD = np.array(df.xD)
+    yD = np.array(df.yD)
+    xF = np.array(df.xF)
+    yF = np.array(df.yF)
+
+    longD, latD = transform(inProj,outProj,xD, yD)
+    longF, latF = transform(inProj,outProj,xF, yF)
+
+    df['longD'] = longD.tolist()
+    df['latD'] = latD.tolist()
+    df['longF'] = longF.tolist()
+    df['latF'] = latF.tolist()
+
+    df = df.drop(['xD', 'yD', 'xF', 'yF'], axis=1)
+    return df
